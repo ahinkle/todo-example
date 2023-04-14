d0de12863d4d7028a4211d6339fd376a90cfea06
Andy Hinkle <ahinkle10@gmail.com>
2023-04-14 12:07:44 -0500

    Implement Tabs

---
+++ src/components/navigation/Tab.tsx
@@ -0,0 +1,10 @@
+interface TabProps {
+  label: string;
+  children: React.ReactNode;
+}
+
+const Tab: React.FC<TabProps> = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Tab;
---
+++ src/components/navigation/Tabs.tsx
@@ -0,0 +1,52 @@
+import { Children, useState, isValidElement } from 'react';
+
+interface TabProps {
+    children: React.ReactNode
+}
+
+const Tabs = ({ children }: TabProps) => {
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabClick = (index: number) => {
+        setActiveTab(index);
+    };
+
+    function classNames(...classes: string[]) {
+        return classes.filter(Boolean).join(' ')
+    }
+
+    return (
+      <div className='w-full'>
+        <div className="hidden sm:block bg-white rounded-lg px-4 pt-4 border">
+            <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                {Children.map(children, (child: any, index: number) => {
+                    if (isValidElement<TabProps>(child)) {
+                        return (
+                            <a
+                                key={index}
+                                onClick={() => handleTabClick(index)}
+                                className={classNames(
+                                    index === activeTab
+                                        ? 'border-indigo-500 text-indigo-600'
+                                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                                    'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium cursor-pointer'
+                                )}
+                                aria-current={index === activeTab ? 'page' : undefined}
+                            >
+                                {child.props.label}
+                            </a>
+                        );
+                    }
+                })}
+            </nav>
+        </div>
+
+        {/* Display the content of the active tab */}
+        <div className="mt-4">
+            {Children.toArray(children)[activeTab]}
+        </div>
+      </div>
+    );
+};
+
+export default Tabs;
